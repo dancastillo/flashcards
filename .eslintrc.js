87244eb9d70df9bb754f8d49e1aad5a62792893a
dan.castillo <dancastillo99@gmail.com>
2022-10-22 20:40:49 -0500

    feat: added eslint husky prettier urql

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      'jsx': true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'no-underscore-dangle': 'off',
+    'react/button-has-type': 'off',
+    'max-len': ['error', { 'code': 150 }],
+  }
+}
