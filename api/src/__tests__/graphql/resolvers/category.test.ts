989b6f9178792f3c47746451aa6ef4893fe3620f
dan.castillo <dancastillo99@gmail.com>
2022-11-30 19:42:58 -0600

    added: resolver/category tests

--- api/src/__tests__/graphql/resolvers/category.test.ts
+++ api/src/__tests__/graphql/resolvers/category.test.ts
@@ -1,13 +1,74 @@
-import { addCategory } from '../../../graphql/resolvers/category'
+import { randomUUID } from 'crypto';
+import { addCategory, getAllCategories } from '../../../graphql/resolvers/category'
 import Category from '../../../models/category';
 
 jest.mock('../../../models/category')
 
 describe('graphql/resolves/category', () => {
+  describe('addCategory', () => {
+    it('should successfully add a category', async () => {
+      const add = await addCategory({ category: 'testing' });
+      expect(add).toBe('ok');
+      expect(Category).toHaveBeenCalledTimes(1);
+      expect(Category).toBeCalledWith({ category: 'testing' });
+    });
+  });
 
-  it('add category', async () => {
-    const add = await addCategory({ category: 'testing' });
-    expect(add).toBe('ok');
-    expect(Category).toHaveBeenCalledTimes(1);
-  })
-})
+  describe('addCategory Error', () => {
+    beforeEach(() => {
+      Category.prototype.save = jest.fn().mockRejectedValue(new Error('Failed'));
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should throw error on addCategory', () => {
+      expect(async () => addCategory({ category: 'testing' })).rejects.toThrow('Failed');
+    });
+  });
+
+  describe('getAllCategories', () => {
+    const categoryIdOne = randomUUID();
+    const categoryIdTwo = randomUUID();
+
+    beforeEach(() => {
+      Category.find = jest.fn().mockResolvedValue([
+        {
+          _id: categoryIdOne,
+          category: 'nodejs',
+        }, {
+          _id: categoryIdTwo,
+          category: 'typescript',
+        }
+      ]);
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should successfully get all categories', async () => {
+      const categories = await getAllCategories();
+
+      expect(categories.length).toBe(2);
+      expect(Category.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+
+  describe('getAllCategories Error', () => {
+    beforeEach(() => {
+      Category.find = jest.fn().mockRejectedValue(new Error('Failed'));
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should throw error on addCategory', () => {
+      expect(async () => getAllCategories()).rejects.toThrow('Failed');
+    });
+  });
+
+});
--- api/src/graphql/resolvers/category.ts
+++ api/src/graphql/resolvers/category.ts
@@ -1,14 +1,18 @@
 import Category, { ICategory } from '../../models/category';
 
 export const getAllCategories = async (): Promise<ICategory[]> => {
-  const categories = await Category.find();
+  try {
+    const categories = await Category.find();
 
-  return categories.length
-    ? categories.map(({ _id, category }: ICategory) => ({
-        id: _id,
-        category,
-      }))
-    : [];
+    return categories.length
+      ? categories.map(({ _id, category }: ICategory) => ({
+          id: _id,
+          category,
+        }))
+      : [];
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const addCategory = async ({ category }) => {
@@ -20,7 +24,6 @@ export const addCategory = async ({ category }) => {
     await categoryObj.save();
     return 'ok';
   } catch (err) {
-    console.error(err);
     throw err;
   }
 };
