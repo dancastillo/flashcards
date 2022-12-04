0d2879822aa18b311ca7cc46033c4e835994a26d
dan.castillo <dancastillo99@gmail.com>
2022-12-04 13:17:49 -0600

    updated: home e2e test

--- app/e2e/home/home.spec.ts
+++ app/e2e/home/home.spec.ts
@@ -21,40 +21,44 @@ test('Home page loads when click on \'Home\'', async ({ page }) => {
   await expect(page).toHaveTitle(/Flashcard App/);
 });
 
-test('Home page loads two flashcards', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.route('**/graphql', route => {
-    route.fulfill({
-      status: 200,
-      body: JSON.stringify({
-        'data': {
-          'flashcards': [
-            {
-              'id': '6372f9415c9b9d0271953000',
-              'question': 'question one',
-              'answer': 'answer one',
-              'category': 'category one',
-              'subcategory': 'subcategory one',
-              '__typename': 'Flashcard'
-            },
-            {
-              'id': '6372f9655c9b9d0271953004',
-              'question': 'question two',
-              'answer': 'answer two',
-              'category': 'category two',
-              'subcategory': null,
-              '__typename': 'Flashcard'
-            },
-          ]
-        }
+test.describe(() => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('**/*', route => {
+      route.fulfill({
+        status: 200,
+        body: JSON.stringify({
+          'data': {
+            'flashcards': [
+              {
+                'id': '6372f9415c9b9d0271953000',
+                'question': 'question one',
+                'answer': 'answer one',
+                'category': 'category one',
+                'subcategory': 'subcategory one',
+                '__typename': 'Flashcard'
+              },
+              {
+                'id': '6372f9655c9b9d0271953004',
+                'question': 'question two',
+                'answer': 'answer two',
+                'category': 'category two',
+                'subcategory': null,
+                '__typename': 'Flashcard'
+              },
+            ]
+          }
+        })
       })
-    })
+    });
   });
-  await new Promise(f => setTimeout(f, 1000));
-  await page.getByText('question one').click();
-  await page.getByText('answer one').click();
-  await page.getByText('question two').click();
-  await page.getByText('answer two').click();
+  
+  test('Home page loads two flashcards', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByText('question one').click();
+    await page.getByText('answer one').click();
+    await page.getByText('question two').click();
+    await page.getByText('answer two').click();
+  });
 });
 
 test('Home page loads no data', async ({ page }) => {
